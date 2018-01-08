ec27b75c9470c405beaa0a92f1b313492b51c299
js4000all <mun4000all@gmail.com>
2018-01-08 23:09:33 +0900

    Option改造

--- JsCommon/Option.cs
+++ JsCommon/Option.cs
@@ -4,132 +4,348 @@ using System.Text;
 
 namespace JsCommon
 {
-    public static class Options
+    public static class Option
     {
-        public static Option<T> Filter<T>(this T v, Predicate<T> test)
-            => test(v) ? Option<T>.Of(v) : Option<T>.None();
-
-        public static Option<T> AsSome<T>(this T v) => Option<T>.AsSome(v);
-    }
-    public abstract class Option<T>
-    {
-        public static Option<T> None() => new Option<T>._None();
-        public static Option<T> AsSome(T v) => new Option<T>.Some(v);
-        public static Option<T> Of(T v) => (v == null) ? None() : AsSome(v);
-
-
-        public abstract Option<U> Map<U>(Func<Some, U> whenSome, Func<U> whenNone = null);
-        public abstract Option<U> FlatMap<U>(Func<Some, Option<U>> whenSome, Func<Option<U>> whenNone = null);
-        public abstract Option<T> Do(Action<T> action);
-        public abstract Option<T> DoWhenNone(Action action);
-        public abstract T OrElse(Func<T> supplier);
-        public abstract T OrElse(Func<Some> supplier);
-
         /// <summary>
-        /// Holds not-null value.
-        /// </summary>
-        public class Some : Option<T>
-        {
-            public readonly T v;
-
-            internal Some(T v)
-            {
-                if(v == null)
-                {
-                    throw new ArgumentNullException();
-                }
-                this.v = v;
-            }
-
-            public override Option<U> Map<U>(Func<Some, U> whenSome, Func<U> whenNone) => Option<U>.AsSome(whenSome(this));
-            public override Option<U> FlatMap<U>(Func<Some, Option<U>> whenSome, Func<Option<U>> whenNone) => whenSome(this);
-            public override Option<T> Do(Action<T> action)
-            {
-                action(v);
-                return this;
-            }
-            public override Option<T> DoWhenNone(Action action) => this;
-            public override T OrElse(Func<T> supplier) => v;
-            public override T OrElse(Func<Some> supplier) => v;
-        }
-        class _None : Option<T>
-        {
-            public override Option<U> Map<U>(Func<Some, U> whenSome, Func<U> whenNone)
-                => (whenNone == null) ? Option<U>.None() : Option<U>.AsSome(whenNone());
-            public override Option<U> FlatMap<U>(Func<Some, Option<U>> whenSome, Func<Option<U>> whenNone)
-                => (whenNone == null) ? Option<U>.None() : whenNone();
-            public override Option<T> Do(Action<T> action) => this;
-            public override Option<T> DoWhenNone(Action action)
-            {
-                action();
-                return this;
-            }
-            public override T OrElse(Func<T> supplier) => supplier();
-            public override T OrElse(Func<Some> supplier) => supplier().v;
-        }
-    }
-    public static class OptionExtension {
-        /// <summary>
-        /// Returns itself when it is Some. Returns Some when it is None.
+        /// 指定した型のNoneを返す。
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="op"></param>
-        /// <param name="other"></param>
         /// <returns></returns>
-        public static Option<T> BindWhenNone<T>(this Option<T> op, T other)
-            => op.BindWhenNone(() => other);
-        public static Option<T> BindWhenNone<T>(this Option<T> op, Option<T> other)
-            => op.BindWhenNone(() => other);
-        public static Option<T> BindWhenNone<T>(this Option<T> op, Func<T> supplier)
-            => op.BindWhenNone(() => Option<T>.AsSome(supplier()));
-        public static Option<T> BindWhenNone<T>(this Option<T> op, Func<Option<T>> supplier)
-            => op.FlatMap(v => v, supplier);
+        public static Option<T> None<T>() => new _None<T>();
+        /// <summary>
+        /// 指定した値が非nullであればSomeにラップし、nullであればNoneを返す。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Option<T> Of<T>(T v) => (v == null) ? None<T>() : new Some<T>(v);
 
         /// <summary>
-        ///  Returns None when it is Some. Returns Some when it is None.
+        /// 指定した値をSome化する。
+        /// nullを指定すると<see cref="ArgumentNullException"/>が発生。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Some<T> AsSome<T>(this T v)
+        {
+            if (v == null)
+            {
+                throw new ArgumentNullException();
+            }
+            return new Some<T>(v);
+        }
+        /// <summary>
+        /// 指定した値が非nullかつ条件をみたす場合、Some化する。
+        /// そうでなければNoneを返す。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="v"></param>
+        /// <param name="test"></param>
+        /// <returns></returns>
+        public static Option<T> AsSomeIf<T>(this T v, Predicate<T> test) => Option.Of(v).Filter(test);
+
+        /// <summary>
+        /// Someを返す関数に変換する。
+        /// </summary>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="f"></param>
+        /// <returns></returns>
+        public static Func<Some<R>> AsSomeSupplier<R>(this Func<R> f)
+            => () => Option.AsSome(f());
+        /// <summary>
+        /// Someを返す関数に変換する。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="f"></param>
+        /// <returns></returns>
+        public static Func<T, Some<R>> AsSomeSupplier<T, R>(this Func<T, R> f)
+            => v => Option.AsSome(f(v));
+    }
+
+    /// <summary>
+    /// 値を持つ場合(Some)と持たない場合(None)を透過的に扱うためのクラス。
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Option<T>
+    {
+        /// <summary>
+        /// 保持する値を変換する。
+        /// SomeならwhenSomeを実行して保持する値を変換する。
+        /// NoneかつwhenNoneを指定した場合は、whenNoneの戻り値を保持するSomeを返す。
+        /// whenSomeおよびwhenNoneの戻り値は非nullでなければならない。Noneを返したい
+        /// 場合は<see cref="Option{T}.FlatMap{U}(Func{T, Option{U}}, Func{Option{U}})"/>を使うこと。
+        /// </summary>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="whenSome"></param>
+        /// <param name="whenNone"></param>
+        /// <returns></returns>
+        public abstract Option<U> Map<U>(Func<T, U> whenSome, Func<U> whenNone = null);
+        /// <summary>
+        /// 他のOptionに置き換える。
+        /// SomeならwhenSomeを実行して、結果のOptionを返す。
+        /// NoneかつwhenNoneを指定した場合は、whenNoneの戻り値を返す。
+        /// </summary>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="whenSome"></param>
+        /// <param name="whenNone"></param>
+        /// <returns></returns>
+        public abstract Option<U> FlatMap<U>(Func<T, Option<U>> whenSome, Func<Option<U>> whenNone = null);
+        /// <summary>
+        /// 処理の実行。
+        /// Someの場合のみ、保持する値を引数として指定した処理を実行し、自身を返す。
+        /// </summary>
+        /// <param name="actionWhenSome"></param>
+        /// <returns></returns>
+        public abstract Option<T> Do(Action<T> actionWhenSome);
+        /// <summary>
+        /// 処理の実行。
+        /// Noneの場合のみ指定した処理を実行し、自身を返す。
+        /// </summary>
+        /// <param name="actionWhenNone"></param>
+        /// <returns></returns>
+        public abstract Option<T> DoWhenNone(Action actionWhenNone);
+
+        /// <summary>
+        /// Some化。
+        /// Someなら自身を返し、Noneなら指定した処理の戻り値を返す。
+        /// </summary>
+        /// <param name="supplierWhenNone"></param>
+        /// <returns></returns>
+        public abstract Some<T> OrElse(Func<Some<T>> supplierWhenNone);
+
+        /// <summary>
+        /// Someなら保持する値がUにキャスト可能な場合のみ、U型のSomeに置き換える。
+        /// Noneなら無条件にU型のNoneに置き換える。
+        /// </summary>
+        /// <typeparam name="U"></typeparam>
+        /// <returns></returns>
+        public Option<U> Cast<U>() => this.Filter(v => v is U).Map(v => (U)(object)v);
+
+
+        /// <summary>
+        /// 内部処理用。whenSomeの引数は自身。
+        /// </summary>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="whenSome"></param>
+        /// <param name="whenNone"></param>
+        /// <returns></returns>
+        internal abstract Option<U> FlatMapMe<U>(Func<Some<T>, Option<U>> whenSome, Func<Option<U>> whenNone = null);
+    }
+    /// <summary>
+    /// Holds not-null value.
+    /// </summary>
+    public class Some<T> : Option<T>
+    {
+        public readonly T v;
+
+        internal Some(T v)
+        {
+            this.v = v;
+        }
+
+        public override Option<U> Map<U>(Func<T, U> whenSome, Func<U> whenNone = null)
+            => FlatMap(whenSome.AsSomeSupplier());
+        public override Option<U> FlatMap<U>(Func<T, Option<U>> whenSome, Func<Option<U>> whenNone = null)
+            => whenSome(v);
+
+        public override Option<T> Do(Action<T> actionWhenSome) => Do2(actionWhenSome);
+        public override Option<T> DoWhenNone(Action actionWhenNone) => this;
+        public override Some<T> OrElse(Func<Some<T>> supplierWhenNone) => this;
+
+        internal override Option<U> FlatMapMe<U>(Func<Some<T>, Option<U>> whenSome, Func<Option<U>> whenNone = null)
+            => whenSome(this);
+
+        /// <summary>
+        /// 無条件にNoneに変換する。
+        /// </summary>
+        /// <returns></returns>
+        public Option<T> Delete() => Option.None<T>();
+        /// <summary>
+        /// 保持する値でactionを呼び出し、自身を返す。
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public Some<T> Do2(Action<T> action)
+        {
+            action(v);
+            return this;
+        }
+        /// <summary>
+        /// actionを呼び出し、自身を返す。
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public Some<T> Do2(Action action) => Do2(_ => action());
+    }
+    class _None<T> : Option<T>
+    {
+        public override Option<U> Map<U>(Func<T, U> whenSome, Func<U> whenNone)
+            => FlatMap(null, whenNone?.AsSomeSupplier());
+        public override Option<U> FlatMap<U>(Func<T, Option<U>> whenSome, Func<Option<U>> whenNone)
+            => (whenNone == null) ? Option.None<U>() : whenNone();
+
+        public override Option<T> Do(Action<T> actionWhenSome) => this;
+        public override Option<T> DoWhenNone(Action actionWhenNone)
+        {
+            actionWhenNone();
+            return this;
+        }
+        public override Some<T> OrElse(Func<Some<T>> supplierWhenNone) => supplierWhenNone();
+
+        internal override Option<U> FlatMapMe<U>(Func<Some<T>, Option<U>> whenSome, Func<Option<U>> whenNone)
+            => FlatMap(null, whenNone);
+    }
+
+    /// <summary>
+    /// <see cref="Option{T}"/>の拡張メソッド。
+    /// </summary>
+    public static class OptionExtension {
+        /// <summary>
+        /// 状態を入れ替える。
+        /// Noneの場合、指定したSomeを返す。
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <typeparam name="U"></typeparam>
         /// <param name="op"></param>
-        /// <param name="supplier"></param>
+        /// <param name="otherWhenNone"></param>
         /// <returns></returns>
-        public static Option<U> BindOnlyWhenNone<T, U>(this Option<T> op, U other)
-            => op.BindOnlyWhenNone(() => other);
-        public static Option<U> BindOnlyWhenNone<T, U>(this Option<T> op, Option<U> other)
-            => op.BindOnlyWhenNone(() => other);
-        public static Option<U> BindOnlyWhenNone<T, U>(this Option<T> op, Func<U> supplier)
-            => op.BindOnlyWhenNone(() => Option<U>.AsSome(supplier()));
-        public static Option<U> BindOnlyWhenNone<T, U>(this Option<T> op, Func<Option<U>> supplier)
-            => op.FlatMap(_ => Option<U>.None(), supplier);
+        public static Option<U> Invert<T, U>(this Option<T> op, Some<U> otherWhenNone)
+            => op.Invert(() => otherWhenNone);
+        /// <summary>
+        /// 状態を入れ替える。
+        /// Noneの場合、指定した処理の戻り値を返す。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="op"></param>
+        /// <param name="supplierWhenNone"></param>
+        /// <returns></returns>
+        public static Option<U> Invert<T, U>(this Option<T> op, Func<Some<U>> supplierWhenNone)
+            => op.FlatMap(_ => Option.None<U>(), supplierWhenNone);
 
-        public static Option<Tuple<T, U>> Concat<T, U>(this Option<T> op, U v)
-            => op.Concat(_ => v);
-        public static Option<Tuple<T, U>> Concat<T, U>(this Option<T> op, Func<T, U> converter)
-            => op.FlatMap(v1 => Option<U>.AsSome(converter(v1.v)).Map(v2 => new Tuple<T, U>(v1.v, v2.v)));
 
+        /// <summary>
+        /// Someなら指定した値とのペアを返す。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="op"></param>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Option<Tuple<T, U>> Concat<T, U>(this Option<T> op, Some<U> v)
+            => op.Map(v1 => new Tuple<T, U>(v1, v.v));
+        /// <summary>
+        /// Someなら保持する値を引数として指定した処理を実行し、その戻り値とのペアを返す。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="op"></param>
+        /// <param name="converter"></param>
+        /// <returns></returns>
+        public static Option<Tuple<T, U>> Concat<T, U>(this Option<T> op, Func<T, Some<U>> converter)
+            => op.FlatMapMe(v1 => converter(v1.v).Map(v2 => new Tuple<T, U>(v1.v, v2)));
+
+
+        /// <summary>
+        /// 現在の値と指定した値の両方がSomeの場合、それらのペアを返す。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="op"></param>
+        /// <param name="other"></param>
+        /// <returns></returns>
         public static Option<Tuple<T, U>> Both<T, U>(this Option<T> op, Option<U> other)
-            => op.Both(v => other);
+            => op.FlatMap(v1 => other.Map(v2 => new Tuple<T, U>(v1, v2)));
+        /// <summary>
+        /// 現在の値がSomeで、かつ保持する値を引数として指定した処理を実行した結果が
+        /// Someである場合、それらのペアを返す。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="op"></param>
+        /// <param name="converter"></param>
+        /// <returns></returns>
         public static Option<Tuple<T, U>> Both<T, U>(this Option<T> op, Func<T, Option<U>> converter)
-            => op.FlatMap(v1 => converter(v1.v).Map(v2 => new Tuple<T, U>(v1.v, v2.v)));
+            => op.Both(op.FlatMap(converter));
 
+        /// <summary>
+        /// Someかつ条件を満たす場合のみ状態を維持する。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="op"></param>
+        /// <param name="test"></param>
+        /// <returns></returns>
         public static Option<T> Filter<T>(this Option<T> op, Predicate<T> test)
-            => op.FlatMap(v => test(v.v) ? v : Option<T>.None());
+            => op.FlatMapMe(some => test(some.v) ? some : some.Delete());
 
-        public static T OrElse<T>(this Option<T> op, T defaultValue)
-            => op.OrElse(() => defaultValue);
-        public static T OrElse<T>(this Option<T> op, Option<T>.Some defaultValue)
-            => op.OrElse(() => defaultValue);
-        public static T OrDefault<T>(this Option<T> op) => op.OrElse(default(T));
+        /// <summary>
+        /// Someの場合のみ指定した処理を実行し、自身を返す。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="op"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static Option<T> Do<T>(this Option<T> op, Action action) => op.Do(_ => action());
 
-        public static Option<T> ThrowWhenSome<T, X>(this Option<T> op, Func<X> exceptionSupplier) where X : Exception
-            => op.Do(_ =>
+
+        /// <summary>
+        /// Some化。
+        /// Noneなら、指定した値を保持するSomeに置き換える。値は非nullでなければならない。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="op"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static Some<T> OrElse<T>(this Option<T> op, T defaultValue)
+            => op.OrElse(Option.AsSome(defaultValue));
+        /// <summary>
+        /// Some化。
+        /// Noneなら指定したSomeに置き換える。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="op"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static Some<T> OrElse<T>(this Option<T> op, Some<T> defaultValue)
+            => op.OrElse(() => defaultValue);
+        /// <summary>
+        /// デフォルト値によってSome化する。デフォルト値は非nullでなければならない。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="op"></param>
+        /// <returns></returns>
+        public static Some<T> OrDefault<T>(this Option<T> op) => op.OrElse(default(T));
+
+
+        /// <summary>
+        /// Someなら、保持する値を引数として指定した処理を実行し、
+        /// 結果の例外を投げる。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="X"></typeparam>
+        /// <param name="op"></param>
+        /// <param name="exceptionSupplier"></param>
+        /// <returns></returns>
+        public static Option<T> Throw<T, X>(this Option<T> op, Func<T, X> exceptionSupplier) where X : Exception
+            => op.Do(v =>
             {
-                throw exceptionSupplier();
+                throw exceptionSupplier(v);
             });
+        /// <summary>
+        /// Noneなら指定した処理の戻り値を投げる。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="X"></typeparam>
+        /// <param name="op"></param>
+        /// <param name="exceptionSupplier"></param>
+        /// <returns></returns>
         public static Option<T> ThrowWhenNone<T, X>(this Option<T> op, Func<X> exceptionSupplier) where X : Exception
             => op.DoWhenNone(() =>
             {
                 throw exceptionSupplier();
             });
+
     }
 }
---
+++ UnitTestProject1/OptionTest.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JsCommon
+{
+    using JsCommon;
+
+    static class OptionTestExtension
+    {
+        public static Some<T> AssertSome<T>(this Option<T> op)
+            => op.IsInstanceOf<Some<T>>();
+        public static Some<T> AssertSome<T>(this Option<T> op, T expected)
+            => op.IsInstanceOf<Some<T>>().Do2(v => v.Is(expected));
+
+        public static Option<T> AssertNone<T>(this Option<T> op)
+        {
+            op.IsNotInstanceOf<Some<T>>();
+            return op;
+        }
+    }
+
+    [TestClass]
+    public class OptionTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AsSome_null()
+            => Option.AsSome<string>(null);
+        [TestMethod]
+        public void AsSome_非null()
+            => "ABC".AsSome().AssertSome("ABC");
+
+        [TestMethod]
+        public void None()
+            => Option.None<string>().AssertNone();
+
+        [TestMethod]
+        public void Of_非null()
+            => Option.Of("ABC").AssertSome("ABC");
+        [TestMethod]
+        public void Of_null()
+            => Option.Of<string>(null).AssertNone();
+
+        [TestMethod]
+        public void Delete()
+            => "ABC".AsSome().Delete().AssertNone();
+
+        [TestMethod]
+        public void Filter_Some_条件満たす()
+            => "ABC".AsSome().Filter(v => v.StartsWith("A")).AssertSome("ABC");
+        [TestMethod]
+        public void Filter_Some_条件満たさない()
+            => "ABC".AsSome().Filter(v => v.StartsWith("B")).AssertNone();
+        [TestMethod]
+        public void Filter_None()
+            => Option.None<string>().Filter(v => v.StartsWith("A")).AssertNone();
+
+        [TestMethod]
+        public void AsSomeIf_条件満たす()
+            => "ABC".AsSomeIf(v => v.StartsWith("A")).AssertSome("ABC");
+        [TestMethod]
+        public void AsSomeIf_条件満たさない()
+            => "ABC".AsSomeIf(v => v.StartsWith("B")).AssertNone();
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Map_Some_戻り値がnull()
+            => "ABC".AsSome().Map<object>(v => null, NotCalled);
+        [TestMethod]
+        public void Map_Some_戻り値が非null()
+            => "ABC".AsSome().Map(v => v.Length, NotCalledI).AssertSome(3);
+        [TestMethod]
+        public void Map_None()
+            => Option.None<string>().Map(NotCalledI, () => 5).AssertSome(5);
+        [TestMethod]
+        public void Map_None_処理なし()
+            => Option.None<string>().Map(NotCalledI).AssertNone();
+
+        [TestMethod]
+        public void FlatMap_Some()
+            => "ABCDE".AsSome().FlatMap(v => v.Length.AsSome(), NotCalledI2).AssertSome(5);
+        [TestMethod]
+        public void FlatMap_None()
+            => Option.None<string>().FlatMap(NotCalledI2, () => 10.AsSome()).AssertSome(10);
+        [TestMethod]
+        public void FlatMap_None_処理なし()
+            => Option.None<string>().FlatMap(NotCalledI2).AssertNone();
+
+        [TestMethod]
+        public void Do_Some()
+        {
+            var some = "ABC".AsSome();
+            string s = null;
+            some
+                .Do(v => s = v)
+                .DoWhenNone(NotCalled3)
+                .IsSameReferenceAs(some);
+            s.Is("ABC");
+        }
+        [TestMethod]
+        public void Do_None()
+        {
+            var none = Option.None<string>();
+            string s = null;
+            none
+                .Do(NotCalled3)
+                .DoWhenNone(() => s = "OK")
+                .IsSameReferenceAs(none);
+            s.Is("OK");
+        }
+
+        [TestMethod]
+        public void OrElse_Some()
+            => "ABC".AsSome().OrElse("EFG").AssertSome("ABC");
+        [TestMethod]
+        public void OrElse_None()
+            => Option.None<string>().OrElse("EFG").AssertSome("EFG");
+
+        [TestMethod]
+        public void Concat_Some()
+        {
+            var some = "ABC".AsSome();
+            var pair = some.Concat(v => (v + "*").AsSome()).AssertSome();
+            pair.v.Item1.Is("ABC");
+            pair.v.Item2.Is("ABC*");
+        }
+        [TestMethod]
+        public void Concat_None()
+            => Option.None<string>().Concat(NotCalled2).AssertNone();
+
+        [TestMethod]
+        public void Both_Option_SomeとSome()
+        {
+            var some = "ABC".AsSome();
+            var pair = some.Both("DEF".AsSome()).AssertSome();
+            pair.v.Item1.Is("ABC");
+            pair.v.Item2.Is("DEF");
+        }
+        [TestMethod]
+        public void Both_Option_SomeとNone()
+            => "ABC".AsSome().Both(Option.None<string>()).AssertNone();
+        [TestMethod]
+        public void Both_Option_None()
+            => Option.None<string>().Both("DEF".AsSome()).AssertNone();
+
+        [TestMethod]
+        public void Both_Func_SomeとSome()
+        {
+            var some = "ABC".AsSome();
+            var pair = some.Both(v => (v + "*").AsSome()).AssertSome();
+            pair.v.Item1.Is("ABC");
+            pair.v.Item2.Is("ABC*");
+        }
+        [TestMethod]
+        public void Both_Func_SomeとNone()
+            => "ABC".AsSome().Both(_ => Option.None<string>()).AssertNone();
+        [TestMethod]
+        public void Both_Func_None()
+            => Option.None<string>().Both(NotCalled2).AssertNone();
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException), "ABC")]
+        public void Throw_Some()
+            => "ABC".AsSome().Throw(v => new IndexOutOfRangeException(v));
+        [TestMethod]
+        public void Throw_None()
+            => Option.None<string>().Throw(v => new IndexOutOfRangeException(v)).AssertNone();
+
+        [TestMethod]
+        public void ThrowWhenNone_Some()
+            => "ABC".AsSome().ThrowWhenNone(() => new IndexOutOfRangeException()).AssertSome();
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void ThrowWhenNone_None()
+            => Option.None<string>().ThrowWhenNone(() => new IndexOutOfRangeException());
+
+
+        [TestMethod]
+        public void Cast_アップキャスト()
+        {
+            Some<B> someOfB = new B().AsSome();
+            Option<A> op = someOfB.Cast<A>();
+            op.AssertSome();
+        }
+        [TestMethod]
+        public void Cast_ダウンキャスト()
+        {
+            Some<A> someOfB = new B().AsSome<A>();
+            Option<B> op = someOfB.Cast<B>();
+            op.AssertSome();
+        }
+        [TestMethod]
+        public void Cast_キャストできない型()
+        {
+            Some<A> someOfB = new B().AsSome<A>();
+            Option<C> op = someOfB.Cast<C>();
+            op.AssertNone();
+        }
+        [TestMethod]
+        public void Cast_None()
+        {
+            Option<A> noneOfA = Option.None<A>();
+            Option<C> op = noneOfA.Cast<C>();
+            op.AssertNone();
+        }
+
+        class A { }
+        class B : A { }
+        class C { }
+
+        static string NotCalled(string s)
+            => s.AsSome().Do2(_ => Assert.Fail()).v;
+        static int NotCalledI(string s)
+            => 0.AsSome().Do2(_ => Assert.Fail()).v;
+
+        static string NotCalled() => NotCalled("dummy");
+        static int NotCalledI() => NotCalledI("dummy");
+        static Some<string> NotCalled2(string s) => NotCalled(s).AsSome();
+        static Some<int> NotCalledI2(string s) => NotCalledI(s).AsSome();
+        static Some<string> NotCalled2() => NotCalled2("dummy");
+        static Some<int> NotCalledI2() => NotCalledI2("dummy");
+        static void NotCalled3(string s) => NotCalled(s).AsSome();
+        static void NotCalled3() => NotCalled3("dummy");
+    }
+}
